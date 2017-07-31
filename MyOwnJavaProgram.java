3116b626afddc4c1530051d2014694a53b4a7471
mblank722 <mitchellblank@sbcglobal.net>
2017-07-31 12:28:53 -0700

    add MyOwnJavaProgram and HelloWorld

---
+++ MyOwnJavaProgram.java
@@ -0,0 +1,7 @@
+public class MyOwnJavaProgram {
+  public static void main(String[] args) {
+    System.out.println("My name is Mitchell Blank");
+    System.out.println("My age is 62");
+    System.out.println("My Hometown is Valley Stream");
+  }
+}
